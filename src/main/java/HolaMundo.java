88fb16fbf03c6fb79264bc254b738b7d18808d95
hansuber <manuel.hans.uber@gmail.com>
2022-10-13 01:01:52 +0200

    Merge branch 'master' of https://github.com/mhansuber/back-end-test.git

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -5,7 +5,8 @@ public class HolaMundo {
 	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Hola Mundo ");
 		Integer i;     
 	}
