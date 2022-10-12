590fea1d2994d7f87c4c56e8d65d9ce5ce69d2d2
hansuber <manuel.hans.uber@gmail.com>
2022-10-13 00:56:46 +0200

    prueba

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -6,7 +6,7 @@ public class HolaMundo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo ");
 		Integer i;     
 	}
 
